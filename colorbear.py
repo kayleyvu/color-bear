b8becb0e37a1a9d98a3c1512119ac92d05d5560d
Kayley Vu <kayleytvu@gmail.com>
2024-01-01 23:37:41 -0600

    Update colorbear.py

--- colorbear.py
+++ colorbear.py
@@ -1,5 +1,5 @@
-# This is a color picker project 
-# using tkinter's color chooser tool
+#This is a color picker project 
+#using tkinter's color chooser tool
 
 #Libraries 
 import tkinter as tk
@@ -20,18 +20,18 @@ h = 300 # Height
 screen_width = root.winfo_screenwidth()  # Width of the screen
 screen_height = root.winfo_screenheight() # Height of the screen
  
-# Calculate Starting X and Y coordinates for Window
+#Calculate Starting X and Y coordinates for Window
 x = (screen_width/2) - (w/2)
 y = (screen_height/2) - (h/2)
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-# Create a Label widget
+#Create a Label widget
 color_label = tk.Label(root, text="ʕ•ᴥ•ʔ", font=("Helvitica", 54))
 color_label.pack(pady=50)
 
-# Create a button to open the color dialog
+#Create a button to open the color dialog
 color_button = tk.Button(root, text="Change my color", command=pick_color)
 color_button.pack()
 
-# Start the Tkinter main loop
+#Start the Tkinter main loop
 root.mainloop()
